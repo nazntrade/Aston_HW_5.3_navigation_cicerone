faf45f8fa3ca5b6c0b2e205690642129161db11a
Andrey Nazentsev <nazntrade@gmail.com>
2023-10-04 15:07:00 +0300

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Aston_HW_5.3_navigation_cicerone"
+include(":app")
+ 
